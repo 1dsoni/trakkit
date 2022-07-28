23717a8682e3741e2638ecac7ec8571c62423473
Diwanshu <diwanshu@zolve.com>
2022-07-28 11:06:02 +0530

    use env in local

--- settings/local.py
+++ settings/local.py
@@ -1,5 +1,20 @@
+import os
+
 from constants import LOCAL_ENV
 from .base import BaseSettings
+
+
+class ENV_VARS:
+    DJANGO_SETTINGS_MODULE = os.environ['DJANGO_SETTINGS_MODULE']
+
+    SECRET_KEY = os.environ['SECRET_KEY']
+    ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+
+    DB_HOST = os.environ['DB_HOST']
+    DB_PORT = os.environ['DB_PORT']
+    DB_USER = os.environ['DB_USER']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    DB_NAME = os.environ['DB_NAME']
 
 
 class LocalSettings(BaseSettings):
@@ -13,11 +28,11 @@ class LocalSettings(BaseSettings):
     DATABASES = {
         'default': {
             'ENGINE': 'dj_db_conn_pool.backends.mysql',
-            'NAME': 'trakkit_db',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '3306',
+            'NAME': ENV_VARS.DB_NAME,
+            'USER': ENV_VARS.DB_USER,
+            'PASSWORD': ENV_VARS.DB_PASSWORD,
+            'HOST': ENV_VARS.DB_HOST,
+            'PORT': ENV_VARS.DB_PORT,
             'CONN_MAX_AGE': 0,
             'POOL_OPTIONS': {
                 'pool_size': 20,
