cbd07a7d774e151af2dc691bfae93ce4f15f0a03
Diwanshu <diwanshu@zolve.com>
2022-07-29 16:18:50 +0530

    try static

--- apps/urls.py
+++ apps/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.http import JsonResponse
 from django.urls import path, include
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path('', include('apps.portfolio.urls')),
     path('', include('apps.documentation.urls')),
 ]
+
+settings.DEBUG = True
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+settings.DEBUG = False
